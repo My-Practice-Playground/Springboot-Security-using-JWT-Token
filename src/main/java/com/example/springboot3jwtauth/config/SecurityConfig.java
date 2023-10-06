9058b317c315da2025892ea414346265bea08ee7
Shan <perera.alc2000@gmail.com>
2023-10-06 23:59:42 +0530

    UserService added

--- src/main/java/com/example/springboot3jwtauth/config/SecurityConfig.java
+++ src/main/java/com/example/springboot3jwtauth/config/SecurityConfig.java
@@ -1,4 +1,59 @@
 package com.example.springboot3jwtauth.config;
 
+import com.example.springboot3jwtauth.filters.JwtAuthFilter;
+import com.example.springboot3jwtauth.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService.userDetailService() );
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http.csrf(csrf-> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"api/v1/signup","api/v1/signin").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/test/**").permitAll()
+                                .anyRequest().authenticated()
+
+                )
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
+
--- src/main/java/com/example/springboot3jwtauth/services/UserService.java
+++ src/main/java/com/example/springboot3jwtauth/services/UserService.java
@@ -1,4 +1,35 @@
 package com.example.springboot3jwtauth.services;
 
+import com.example.springboot3jwtauth.models.User;
+import com.example.springboot3jwtauth.repositories.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepo userRepo;
+
+    public UserDetailsService userDetailsService() {
+        return new UserDetails() {
+            @Override
+            public UserDetails loadUserByUsername(String email) {
+                return userRepo.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
+            }
+        };
+    }
+
+    public User save(User newUser) {
+        if (newUser.getId() == null) {
+            newUser.setCreatedAt(LocalDateTime.now());
+        }
+        newUser.setUpdatedAt(LocalDateTime.now());
+        return userRepo.save(newUser);
+    }
 }
+
