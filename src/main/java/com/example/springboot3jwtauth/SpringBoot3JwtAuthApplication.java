96a2937c24fb3ef15dff2d62b35616b1a9151111
Shan <perera.alc2000@gmail.com>
2023-10-06 23:07:18 +0530

    initial commit

---
+++ src/main/java/com/example/springboot3jwtauth/SpringBoot3JwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3jwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3JwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3JwtAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot3jwtauth/SpringBoot3JwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3jwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
