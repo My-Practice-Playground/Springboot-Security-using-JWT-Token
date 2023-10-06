33254ee2f99d36d27338f3101478b33cd80ae48b
Shan <perera.alc2000@gmail.com>
2023-10-06 23:19:22 +0530

    User entity & Enum Added

---
+++ src/main/java/com/example/springboot3jwtauth/models/User.java
@@ -0,0 +1,63 @@
+package com.example.springboot3jwtauth.models;
+
+import com.example.springboot3jwtauth.util.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@ToString
+public class User implements UserDetails {
+    String firstName;
+    String lastName;
+    @Column(unique = true)
+    String email;
+    String password;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+    @Enumerated(EnumType.STRING)
+    Role role;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/example/springboot3jwtauth/util/Role.java
@@ -0,0 +1,5 @@
+package com.example.springboot3jwtauth.util;
+
+public enum Role {
+    ROLE_ADMIN,ROLE_USER
+}
